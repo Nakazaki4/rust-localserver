65b25bda6e1cb90a9cba11f694434f3f294d43f6
Nakazaki4 <nakazakinakazaki22@gmail.com>
2026-07-10 21:16:32 +0100

    refactor: update config parsing to return a vector of ServerConfig

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-mod server;
 mod config;
+mod server;
 fn main() {
     // first, parse the config file
+    match config::parse_config("./config.conf") {
+        Ok(servers) => 
+        servers.iter().for_each(|s| {
+            server::
+        })
+        ,
+        Err(e) => panic!("{}", format!("an error occured: {e}"))
+    }
+    
     // then create the TcpListeners for each server
 }
