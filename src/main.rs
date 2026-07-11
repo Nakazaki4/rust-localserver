9fdb51c273587e08d54451477a3e7d704d34adc4
Nakazaki4 <nakazakinakazaki22@gmail.com>
2026-07-11 16:29:01 +0100

    save

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+use crate::server::Server;
+
 mod config;
 mod server;
 fn main() {
     // first, parse the config file
-    match config::parse_config("./config.conf") {
-        Ok(servers) => 
-        servers.iter().for_each(|s| {
-            server::
-        })
-        ,
-        Err(e) => panic!("{}", format!("an error occured: {e}"))
-    }
-    
     // then create the TcpListeners for each server
+    // run the loop of listening to requests 
+    match config::parse_config("./config.conf") {
+        Ok(servers) => {
+            servers.iter().enumerate().for_each(
+                |(index, server_conf)| {
+                Server::bind(server_conf.clone(), index).unwrap();
+            });
+        }
+        Err(e) => panic!("{}", format!("error parsing your config file: {}", e)),
+    }
+
 }
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,11 @@
 use crate::config::{Route, ServerConfig};
-use mio::{Poll, Token};
+use mio::net::TcpListener;
+use mio::{Interest, Poll, Token};
 use std::collections::HashMap;
-use std::net::{TcpListener, TcpStream};
+use std::net::{SocketAddr, TcpStream};
 use std::time::Instant;
+
+const SERVER_TOKEN_MAX: usize = 100;
 
 pub struct Server {
     routes: Vec<Route>,
@@ -33,4 +36,50 @@ struct Connection {
     pub last_activity: Instant,
     pub read_buffer: Vec<u8>,
     pub is_request_complete: bool,
+}
+
+impl Server {
+    fn new(config: ServerConfig) -> Self {
+        Server {
+            routes: Vec::new(),
+            poll: Poll::new().expect("failed to create mio poll"),
+            config: config,
+            listeners: Box::new(HashMap::new()),
+            connections: Box::new(HashMap::new()),
+            pending_cgi: Box::new(HashMap::new()),
+            next_token: SERVER_TOKEN_MAX,
+        }
+    }
+
+    pub fn bind(sc: ServerConfig, idx: usize) -> Result<(), String> {
+        let mut server = Self::new(sc);
+
+        let addr = format!("{}:{}", server.config.host, server.config.port)
+            .parse()
+            .map_err(|e| format!("Invalid address: {}", e))?;
+        match TcpListener::bind(addr) {
+            Ok(mut listener) => {
+                let token = Token(idx);
+
+                server
+                    .poll
+                    .registry()
+                    .register(&mut listener, token, Interest::READABLE)
+                    .map_err(|e| e.to_string())?;
+
+                server.listeners.insert(
+                    token,
+                    ListenerEntry {
+                        listener,
+                        server_idx: idx,
+                    },
+                );
+            }
+            Err(e) => eprintln!("failed to bind {}: {}", addr, e),
+        }
+        if server.listeners.is_empty() {
+            return Err("no ports were found".to_string());
+        }
+        Ok(())
+    }
 }
